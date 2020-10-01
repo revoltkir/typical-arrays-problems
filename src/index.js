5a356f6049fe772ba57d0138428a283b9025a11e
Kirill Tikach <70844215+revoltkir@users.noreply.github.com>
2020-10-01 15:23:15 +0300

    feat: add function solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length===0) return 0
+  return Math.min( ...array)
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length===0) return 0
+  return Math.max.apply(null,array);
+
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length===0) return 0
+  
+    return array.reduce((a, b) => (a + b)) / array.length;
 }
+  
